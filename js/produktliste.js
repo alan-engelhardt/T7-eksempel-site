9a7b609c37484a4256f577768b7e40ab88e7d031
Alan <ale@kea.dk>
2023-02-05 22:34:41 +0100

    forEach i fetch-funktion

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,31 +1,28 @@
-const url = `https://kea-alt-del.dk/t7/api/products?limit=50&start=2`;
+const url = `https://kea-alt-del.dk/t7/api/products?limit=20&start=200`;
+const skabelon = document.querySelector("template").content;
+const container = document.querySelector("main");
 
 function getData() {
   fetch(url)
     .then((res) => res.json())
-    .then(visProdukter);
+    .then((produkter) => produkter.forEach(visProdukter));
 }
 
-function visProdukter(data) {
-  const skabelon = document.querySelector("template").content;
-  const container = document.querySelector("main");
-  data.forEach((produkt) => {
-    const kopi = skabelon.cloneNode(true);
-    console.log({ container });
-    kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
-    kopi.querySelector("img").alt = produkt.productdisplayname;
-    kopi.querySelector("h3").textContent = produkt.productdisplayname;
-    kopi.querySelector(".price").textContent = produkt.price;
-    if (produkt.soldout) {
-      kopi.querySelector("article").classList.add("soldOut");
-    }
-    if (produkt.discount) {
-      kopi.querySelector("article").classList.add("onSale");
-      kopi.querySelector(".discounted p").textContent = Math.round(produkt.price - (produkt.price * produkt.discount) / 100);
-      kopi.querySelector(".discounted p+p span").textContent = produkt.discount;
-    }
-    container.appendChild(kopi);
-  });
+function visProdukter(produkt) {
+  const kopi = skabelon.cloneNode(true);
+  kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+  kopi.querySelector("img").alt = produkt.productdisplayname;
+  kopi.querySelector("h3").textContent = produkt.productdisplayname;
+  kopi.querySelector(".price span").textContent = produkt.price;
+  if (produkt.soldout) {
+    kopi.querySelector("article").classList.add("soldOut");
+  }
+  if (produkt.discount) {
+    kopi.querySelector("article").classList.add("onSale");
+    kopi.querySelector(".discounted p span").textContent = Math.round(produkt.price - (produkt.price * produkt.discount) / 100);
+    kopi.querySelector(".discounted p+p span").textContent = produkt.discount;
+  }
+  container.appendChild(kopi);
 }
 
 getData();
