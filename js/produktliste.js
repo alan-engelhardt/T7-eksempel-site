798407ccbdd84f35daa0f42fe53f8f51a42dfccd
Alan <ale@kea.dk>
2023-02-07 11:28:20 +0100

    slet ubrugte articles

--- js/produktliste.js
+++ js/produktliste.js
@@ -10,7 +10,8 @@ function getData() {
 
 function visProdukter(produkt) {
   const kopi = skabelon.cloneNode(true);
-  kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+  const imgurl = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+  kopi.querySelector("img").src = imgurl;
   kopi.querySelector("img").alt = produkt.productdisplayname;
   kopi.querySelector("h3").textContent = produkt.productdisplayname;
   kopi.querySelector(".price span").textContent = produkt.price;
@@ -19,7 +20,9 @@ function visProdukter(produkt) {
   }
   if (produkt.discount) {
     kopi.querySelector("article").classList.add("onSale");
+
     kopi.querySelector(".discounted p span").textContent = Math.round(produkt.price - (produkt.price * produkt.discount) / 100);
+
     kopi.querySelector(".discounted p+p span").textContent = produkt.discount;
   }
   container.appendChild(kopi);
