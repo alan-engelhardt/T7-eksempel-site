f890db1622d96f86d0ee767259f92ec9fed3a5ba
Alan <ale@kea.dk>
2024-08-20 19:01:58 +0200

    category filter

--- js/produktliste.js
+++ js/produktliste.js
@@ -3,27 +3,44 @@ window.addEventListener("DOMContentLoaded", hentData);
 const url = `https://kea-alt-del.dk/t7/api/products?start=10&limit=15`;
 const skabelon = document.querySelector("template").content;
 const container = document.querySelector("main");
+const knapper = document.querySelectorAll("button");
 let data;
+let filter = "alle";
+
+knapper.forEach((knap) => knap.addEventListener("click", filtrer));
 
 function hentData() {
   fetch(url)
     .then((res) => res.json())
     .then((produkter) => {
       data = produkter;
-      const rest = data.filter((elm) => !elm.soldout);
-      visProdukter(rest);
+      visProdukter();
     });
 }
 
-function visProdukter(produkter) {
-  console.log(produkter);
-  produkter.forEach((produkt) => {
+function filtrer() {
+  filter = this.dataset.kat;
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+  console.log(filter);
+  visProdukter();
+}
+
+function visProdukter() {
+  console.log(data);
+  container.textContent = "";
+  data.forEach((produkt) => {
     const kopi = skabelon.cloneNode(true);
-    kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
-    kopi.querySelector("img").alt = produkt.productdisplayname;
-    kopi.querySelector("h3").textContent = produkt.productdisplayname;
-    kopi.querySelector(".price span").textContent = produkt.price;
-    kopi.querySelector("a").href = `product.html?id=${produkt.id}`;
-    container.appendChild(kopi);
+    if (filter == "alle" || produkt.category == filter) {
+      kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+      kopi.querySelector("img").alt = produkt.productdisplayname;
+      kopi.querySelector("h3").textContent = produkt.productdisplayname;
+      kopi.querySelector(".price span").textContent = produkt.price;
+      kopi.querySelector("a").href = `product.html?id=${produkt.id}`;
+      if (produkt.soldout) {
+        kopi.querySelector("article").classList.add("soldOut");
+      }
+      container.appendChild(kopi);
+    }
   });
 }
