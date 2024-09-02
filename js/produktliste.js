b66600471ef41e94ad8ada1e17dfbadfdc8eea31
Alan <ale@kea.dk>
2024-09-02 20:39:43 +0200

    to andre endpoint eksempler

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,4 +1,7 @@
 const url = `https://kea-alt-del.dk/t7/api/products`;
+//const url = `https://cat-fact.herokuapp.com/facts/`;
+//const url = `https://api.tvmaze.com/search/shows?q=postman`;
+
 const skabelon = document.querySelector("template").content;
 const container = document.querySelector("main");
 
@@ -10,11 +13,13 @@ function hentData() {
 
 function visProdukter(produkter) {
   produkter.forEach((produkt) => {
+    console.log(produkt);
     const kopi = skabelon.cloneNode(true);
     kopi.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
     kopi.querySelector("img").alt = produkt.productdisplayname;
     kopi.querySelector("h3").textContent = produkt.productdisplayname;
     kopi.querySelector(".price span").textContent = produkt.price;
+    kopi.querySelector("a").href = `product.html?id=${produkt.id}`;
     container.appendChild(kopi);
   });
 }
